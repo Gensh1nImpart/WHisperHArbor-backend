6f5dd752c3f0d31851e543f9aaf55e203bac5033
xia0ne <1223605525@qq.com>
2023-10-15 13:21:28 +0800

    test: 鉴权测试

--- router.go
+++ router.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"WHisperHArbor-backend/controller"
+	"WHisperHArbor-backend/middleware"
+	"WHisperHArbor-backend/utils"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func MyRouter(r *gin.Engine) {
@@ -10,5 +14,14 @@ func MyRouter(r *gin.Engine) {
 	{
 		authapi.POST("/login", controller.Login)
 		authapi.POST("/register", controller.Register)
+	}
+	authorized := r.Group("/v1/api", middleware.JWTAuth)
+	{
+		authorized.GET("/sayhello", func(c *gin.Context) {
+			auth := c.Request.Header.Get("Authorization")
+			claims, _ := utils.ParseToken(auth)
+			log.Println(claims)
+			c.String(http.StatusOK, "hello"+claims.User.Account)
+		})
 	}
 }
