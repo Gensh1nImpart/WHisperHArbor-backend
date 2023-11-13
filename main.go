252f80b7265d33ce45b6bfad0f74a7391f776edb
xia0ne <1223605525@qq.com>
2023-11-13 15:26:23 +0800

    fix: 修复了没有日志的bug close #8

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"WHisperHArbor-backend/model"
 	"WHisperHArbor-backend/service"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
+	"time"
 )
 
 func init() {
@@ -15,6 +19,14 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	now := time.Now()
+	dateString := now.Format("2006-01-02")
+	logFileName := fmt.Sprintf("%s/%v-log.log", "logs", dateString)
+	f, err := os.Create(logFileName)
+	if err != nil {
+		panic("can create log file")
+	}
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 	MyRouter(r)
 	r.Run(":" + model.MyConfig.Base.Port)
 }
